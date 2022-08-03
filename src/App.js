db2639dbe5e8754144bde3869d5d2aad4c966566
Chhorvy Om <c_om89@live.com>
2022-08-03 13:55:12 -0700

    Imported NasaPhoto into App js. Set data and useEffect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import "./App.css";
 
+import NasaPhoto from './Components/NasaPhoto';
+
 const dummyData = {
   date: "2022-08-03",
   explanation: "What created the unusual halo around the Cat's Eye nebula? No one is sure. What is sure is that the Cat's Eye Nebula (NGC 6543) is one of the best known planetary nebulae on the sky.  Although haunting symmetries are seen in the bright central region, this image was taken to feature its intricately structured outer halo, which spans over three light-years across.  Planetary nebulae have long been appreciated as a final phase in the life of a Sun-like star. Only recently however, have some planetaries been found to have expansive halos, likely formed from material shrugged off during earlier puzzling episodes in the star's evolution. While the planetary nebula phase is thought to last for around 10,000 years, astronomers estimate the age of the outer filamentary portions of the Cat's Eye Nebula's halo to be 50,000 to 90,000 years.",
@@ -16,16 +18,14 @@ function App() {
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
     .then(res => {
-      console.log(res);
+      setData(res.data);
     })
     .catch(err => console.error(err))
   }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      { data && <NasaPhoto photo={data} /> }
     </div>
   );
 }
